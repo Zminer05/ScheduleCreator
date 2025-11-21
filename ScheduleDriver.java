3dc371f35f8115d2ef8628a190c5a4738b66e77b
Zminer05 <cjb6671@uncw.edu>
2025-11-21 16:13:43 -0500

    new commit

---
+++ ScheduleDriver.java
@@ -0,0 +1,22 @@
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ScheduleDriver extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("GroupProject.fxml"));
+        Scene scene = new Scene(loader.load());
+
+        stage.setScene(scene);
+        stage.setTitle("Schedule");
+        stage.show();
+    }
+}
